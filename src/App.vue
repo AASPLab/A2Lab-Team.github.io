d653b5dc7b6fd4dbdd266673269dcc0f59fbe926
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-05-09 19:05:33 -0300

    Create LayoutFooter component ~incomplete

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <h1>A2Lab</h1>
+  <layout-footer />
 </template>
+
+<script>
+  import LayoutFooter from '@/components/LayoutFooter'
+
+  export default {
+    components: { LayoutFooter },
+  }
+</script>
 
 <style lang="stylus" src="@/assets/styles/base.styl"></style>
---
+++ src/components/LayoutFooter.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer class="layout-footer">
+    <section class="media">
+      <h3 class="title">Mídias</h3>
+    </section>
+  </footer>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="stylus">
+  $layout-footer-media
+    &
+      padding-top: 7.5px
+
+    > .title
+      margin: 0
+      color: #fff
+      font-size: 2rem
+      font-weight: normal
+      text-align: center
+      text-transform: uppercase
+
+  .layout-footer
+    &
+      height: 155px
+      background-color: #333333
+
+    > .media
+      @extends $layout-footer-media
+</style>
